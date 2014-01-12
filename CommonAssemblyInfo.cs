fa8fc61dde98a9352fe7ea78553c22dfafc1fdda
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2014-01-12 23:16:08 +0200

    Added NuGet pacakges

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,11 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tech Fellow Consulting")]
 [assembly: AssemblyProduct("FeatureToggle")]
-[assembly: AssemblyCopyright("Copyright © 2013-2014")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- FeatureToggle/Properties/AssemblyInfo.cs
+++ FeatureToggle/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("FeatureToggle")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core library of FeatureToggle")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4fee8c20-2c80-4cd5-a75a-ca7e506eb3bc")]
 [assembly: InternalsVisibleTo("FeatureToggle.Tests")]
