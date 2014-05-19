74b92c6a117bbcfc5838fefef60a54ccad7372f1
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2014-05-19 07:41:41 +0300

    Updated dependencies

--- FeatureSwitch.Web.Optimization/Properties/AssemblyInfo.cs
+++ FeatureSwitch.Web.Optimization/Properties/AssemblyInfo.cs
@@ -5,5 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("FeatureSwitch helper library for System.Web.Optimization")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6ea10b61-5500-4dc9-aa18-682c319e7320")]
-[assembly: AssemblyVersion("1.1.0.2")]
-[assembly: AssemblyFileVersion("1.1.0.2")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
--- FeatureSwitch/Properties/AssemblyInfo.cs
+++ FeatureSwitch/Properties/AssemblyInfo.cs
@@ -7,5 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("4fee8c20-2c80-4cd5-a75a-ca7e506eb3bc")]
 [assembly: InternalsVisibleTo("FeatureSwitch.Tests")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
