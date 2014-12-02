6540b3c4049f61229fedfb239fef986365d501fb
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2014-12-02 09:37:29 +0200

    Fixed removed references
    Sometimes just some weird stuff happens

---
+++ FeatureSwitch.Glimpse/Tab.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Web;
+using FeatureSwitch.Strategies.Implementations;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Tab.Assist;
+
+namespace FeatureSwitch.Glimpse
+{
+    public class Tab : TabBase<HttpContextBase>, ITabLayout, IKey
+    {
+        private static readonly object _layout = TabLayout.Create().Row(r =>
+        {
+            r.Cell("enabled").WidthInPixels(150);
+            r.Cell("name");
+        }).Build();
+
+        public override string Name
+        {
+            get
+            {
+                return "Feature Switch";
+            }
+        }
+
+        public override RuntimeEvent ExecuteOn
+        {
+            get
+            {
+                // we need check if Session strategy is used - then plug-in should be run once session access event fired
+                return FeatureContext.IsStrategyEnabled<HttpSessionStrategyImpl>() ? RuntimeEvent.EndSessionAccess : RuntimeEvent.EndRequest;
+            }
+        }
+        public string Key
+        {
+            get
+            {
+                return "glimpse_featureswitch";
+            }
+        }
+
+        public object GetLayout()
+        {
+            return _layout;
+        }
+
+        public override object GetData(ITabContext context)
+        {
+            var features = FeatureContext.GetFeatures();
+            var vm = features.Select(f => new { Enabled = FeatureContext.IsEnabled(f), f.Name }).ToList();
+            return vm;
+        }
+    }
+
+
+}
