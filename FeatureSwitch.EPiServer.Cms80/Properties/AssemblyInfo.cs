5c15bb8efd06b234d2ad7b2e30ce0455c400da09
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2015-09-05 22:32:46 +0300

    Removed old EPiServer 6.0 project.

---
+++ FeatureSwitch.EPiServer.Cms80/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FeatureSwitch.EPiServer")]
+[assembly: AssemblyDescription("FeatureSwitch integration library for EPiServer 8.x CMS platform.")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8814689b-3a7a-47c5-b5c9-3141cd17340d")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyInformationalVersion("2.4.0")]
--- FeatureSwitch.EPiServer.Cms80/Strategies/EPiServerSiteAttribute.cs
+++ FeatureSwitch.EPiServer.Cms80/Strategies/EPiServerSiteAttribute.cs
@@ -4,8 +4,6 @@ namespace FeatureSwitch.EPiServer.Strategies
 {
     public class EPiServerSiteAttribute : FeatureStrategyAttribute
     {
-        public string Site { get; set; }
-
         public override Type DefaultImplementation
         {
             get { return typeof (EPiServerSiteStrategyImpl); }
