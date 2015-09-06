85a9d36296391c8ef238ee8f6a46e8fd47bcecdc
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2015-09-06 19:00:19 +0300

    Added role based strategy. This closes #18.

---
+++ FeatureSwitch.AspNet.Mvc5/AuthorizedForAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FeatureSwitch.AspNet.Mvc
+{
+    public class AuthorizedForAttribute : FeatureStrategyAttribute
+    {
+        public override Type DefaultImplementation
+        {
+            get { return typeof (AuthorizedForStrategyImpl); }
+        }
+    }
+}
---
+++ FeatureSwitch.AspNet.Mvc5/AuthorizedForStrategyImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Web.Security;
+using FeatureSwitch.Strategies.Implementations;
+
+namespace FeatureSwitch.AspNet.Mvc
+{
+    public class AuthorizedForStrategyImpl : BaseStrategyReaderImpl
+    {
+        public override bool Read()
+        {
+            var availableForRoles = Context.Key.Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim());
+            return availableForRoles.Select(Roles.IsUserInRole).Any(v => v);
+        }
+    }
+}
